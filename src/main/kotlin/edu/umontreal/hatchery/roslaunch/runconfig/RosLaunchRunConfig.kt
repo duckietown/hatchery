be26959409513e98cf71d2931b8dcad39770de5a
breandan <bre@ndan.co>
2019-07-17 07:12:52 -0400

    add persistence for run configurations

--- src/main/kotlin/edu/umontreal/hatchery/roslaunch/runconfig/RosLaunchRunConfig.kt
+++ src/main/kotlin/edu/umontreal/hatchery/roslaunch/runconfig/RosLaunchRunConfig.kt
@@ -7,18 +7,26 @@ import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.openapi.project.Project
 import edu.umontreal.hatchery.cli.RosCommandLineState
 import edu.umontreal.hatchery.settings.RosConfig
+import org.jdom.Element
 
 class RosLaunchRunConfig : LocatableConfigurationBase<RunProfileState> {
   constructor(project: Project, name: String) :
     super(project, RosLaunchRunConfigFactory, name)
 
   var rosLaunchPath = ""
+  val ROS_LAUNCH_PATH = "ros-launch-path"
   var rosPackagePath = ""
+  val ROS_PACKAGE_PATH = "ros-package-path"
 
   var destinationAddress = ""
+  val DESTINATION_ADDRESS = "destination-address"
   var destinationPath = ""
+  val DESTINATION_PATH = "destination-path"
   var rosLaunchOptions: String = RosConfig.settings.defaultRosLaunchOptions
+  val ROS_LAUNCH_OPTIONS = "ros-launch-options"
   var rosLaunchArgs: String = ""
+  val ROS_LAUNCH_ARGS = "ros-launch-args"
+
 
   override fun getConfigurationEditor() =
     RosLaunchSettingsEditor(project, rosPackagePath, rosLaunchPath)
@@ -31,5 +39,25 @@ class RosLaunchRunConfig : LocatableConfigurationBase<RunProfileState> {
         rosLaunchOptions,
         rosLaunchArgs).toString()
     )
+
+  override fun writeExternal(element: Element) {
+    super.writeExternal(element)
+    element.setAttribute(ROS_LAUNCH_PATH, rosLaunchPath)
+    element.setAttribute(ROS_PACKAGE_PATH, rosPackagePath)
+    element.setAttribute(DESTINATION_ADDRESS, destinationAddress)
+    element.setAttribute(DESTINATION_PATH, destinationPath)
+    element.setAttribute(ROS_LAUNCH_OPTIONS, rosLaunchOptions)
+    element.setAttribute(ROS_LAUNCH_ARGS, rosLaunchArgs)
+  }
+
+  override fun readExternal(element: Element) {
+    super.readExternal(element)
+    rosLaunchPath = element.getAttributeValue(ROS_LAUNCH_PATH)
+    rosPackagePath = element.getAttributeValue(ROS_PACKAGE_PATH)
+    destinationAddress = element.getAttributeValue(DESTINATION_ADDRESS)
+    destinationPath = element.getAttributeValue(DESTINATION_PATH)
+    rosLaunchOptions = element.getAttributeValue(ROS_LAUNCH_OPTIONS)
+    rosLaunchArgs = element.getAttributeValue(ROS_LAUNCH_ARGS)
+  }
 //    RunAnythingRunProfileState(environment, RosConfig.settings.localRos.launch(rosPackagePath, rosLaunchPath).toString())
 }
